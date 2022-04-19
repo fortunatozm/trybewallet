82b4257880fcfccae4d15509f249fe1fd77cc270
fortunatozm <fortunatompongo@hotmail.com>
2022-04-19 14:54:08 -0300

    5. Desenvolva um formulário para adicionar uma despesa contendo as seguintes características: - OK

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -26,6 +26,49 @@ class Wallet extends React.Component {
             {/* BRL */}
           </span>
         </header>
+        <section>
+          <label htmlFor="valor">
+            Valor:
+            <input id="valor" data-testid="value-input" />
+          </label>
+
+          <label htmlFor="moeda">
+            Moeda:
+            <select id="moeda" data-testid="currency-input">
+              { currencyM.map((currency) => (
+                <option value={ currency } key={ Math.random() }>
+                  { currency }
+                </option>
+              )) }
+            </select>
+          </label>
+
+          <label htmlFor="metodopag">
+            Método de Pagamento:
+            <select id="metodopag" data-testid="method-input">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+
+          <label htmlFor="categoria">
+            Método de Pagamento:
+            <select id="categoria" data-testid="tag-input">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+
+          <label htmlFor="descricao">
+            Descrição
+            <input id="descricao" data-testid="description-input" />
+          </label>
+
+        </section>
       </div>);
   }
 }
