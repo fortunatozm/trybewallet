c3d5a483f5f853f275e532bf2db7c45f6ba5edcf
fortunatozm <fortunatompongo@hotmail.com>
2022-04-22 13:27:54 -0300

    8. Implemente a lógica para que a tabela seja alimentada pelo estado da aplicação - Erro Lint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -39,7 +39,7 @@ class Wallet extends React.Component {
 
   async hendleClick() {
     const { valor, moeda, metPg, categoria, descricao, identity } = this.state;
-    const { walletD, cotacoesD, cotacoesM } = this.props;
+    const { walletD, cotacoesD } = this.props;
     // const { cotacoesD } = this.props;
     const answer = await fetch('https://economia.awesomeapi.com.br/json/all');
     const response = await answer.json();
@@ -57,8 +57,6 @@ class Wallet extends React.Component {
       exchangeRates: moedaCotada,
     };
     walletD(data);
-    console.log(cotacoesM);
-    console.log(moedaCotada);
 
     this.setState({
       valor: 0,
@@ -205,6 +203,39 @@ class Wallet extends React.Component {
                 Editar/Excluir
               </th>
             </tr>
+            { wallet.map((addData) => (
+              <tr key={ addData.id }>
+                <td>
+                  { addData.description }
+                </td>
+                <td>
+                  { addData.tag }
+                </td>
+                <td>
+                  { addData.method }
+                </td>
+                <td>
+                  { (parseFloat(addData.value)).toFixed(2) }
+                </td>
+                <td>
+                  { (addData.exchangeRates[addData.currency].name).split('/')[0] }
+                </td>
+                <td>
+                  { parseFloat(addData.exchangeRates[addData.currency].ask).toFixed(2) }
+                </td>
+                <td>
+                  { (parseFloat(addData.value)
+              * parseFloat((addData.exchangeRates[addData.currency].ask))).toFixed(2) }
+                </td>
+                <td>
+                  Real
+                </td>
+                <td>
+                  <button type="button">Editar</button>
+                  <button type="button">Excluir</button>
+                </td>
+              </tr>
+            )) }
           </table>
         </section>
       </div>);
@@ -217,7 +248,7 @@ Wallet.propTypes = {
   currencyD: PropTypes.func.isRequired,
   walletD: PropTypes.func.isRequired,
   cotacoesD: PropTypes.func.isRequired,
-  cotacoesM: PropTypes.shape().isRequired,
+  // cotacoesM: PropTypes.shape().isRequired,
   wallet: PropTypes.shape().isRequired,
   // currency: PropTypes.string.isRequired,
 };
